72b3358d5946987d548108032fe2ca332a35d2cf
spamtactics <108055980+spamtactics@users.noreply.github.com>
2024-01-06 20:50:02 +0000

    Added the template for the Wakizashi counter ability
    Actually calling the methods would probably have to be within the player script.

--- Dimension Hopper/Assets/Scripts/Weapon Abilities/Wakizashi_Counter.cs
+++ Dimension Hopper/Assets/Scripts/Weapon Abilities/Wakizashi_Counter.cs
@@ -1,37 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
 public class Wakizashi_Counter : MonoBehaviour
 {
-    public float currentCooldown;
-
-    public float baseCooldown;
-
-    public float currentAbilityDuration;
-
-    public float baseAbilityDuration;
-
-    public bool succesfulCounter;
-
+    public double currentCooldown;
+    public double baseCooldown;
+    public double currentAbilityDuration;
+    public double baseAbilityDuration;
     public bool abilityUsed;
+    public double cooldownReduction;
+    //initialise a timer object
+    
     // Start is called before the first frame update
-    
-    
     void Start()
     {
         currentCooldown = 0;
-        
+        abilityUsed = false;
     }
-
     // Update is called once per frame
-    void TriggerAbility()
-    {
-        abilityUsed = true;
-        currentAbilityDuration = 0;
-        
-    }
     void Update()
     {
-        
+        if (abilityUsed)
+        {
+            currentAbilityDuration = currentAbilityDuration - Time.deltaTime;
+        }
+        else if(currentCooldown>0)
+        {
+            currentCooldown = currentCooldown - Time.deltaTime;
+        }
+    }
+    void TriggerAbility()
+    {
+        if (currentCooldown <= 0)
+        {
+            currentAbilityDuration = baseAbilityDuration;
+        }
+        else
+        {
+            // return some sort of error
+        }
+    }
+    
+    void SuccessfulCounter()
+    {
+        currentCooldown = baseCooldown - cooldownReduction;
+        abilityUsed = false;
+    }
+
+    double getCurrentDuration()
+    {
+        return currentAbilityDuration;
+    }
+    void abilityEnd()
+    {
+        abilityUsed = false;
+        currentCooldown = baseCooldown;
     }
 }
