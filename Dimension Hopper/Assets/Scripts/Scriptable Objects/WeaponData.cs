7b19489988b8fce911269df78be791a241a5bff6
spamtactics <108055980+spamtactics@users.noreply.github.com>
2024-01-10 15:21:22 +0000

    Created the enemy attributes

--- Dimension Hopper/Assets/Scripts/Scriptable Objects/WeaponData.cs
+++ Dimension Hopper/Assets/Scripts/Scriptable Objects/WeaponData.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName="MeleeWeapon", menuName="Weapon/Melee",order=2)]
+[CreateAssetMenu(fileName="MeleeWeapon", menuName="Weapon/Melee", order=1)]
 public class WeaponData : ScriptableObject {
     [Header("Info")]
     public string weaponName;
